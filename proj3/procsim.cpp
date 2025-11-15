07956102f50e4e0600f180c42dd68024fab4dead
Kevin Sheng <kevinsheng2005@gmail.com>
2025-11-14 16:27:55 -0800

    this doesn't even work

--- proj3/procsim.cpp
+++ proj3/procsim.cpp
@@ -1,8 +1,57 @@
 #include "procsim.hpp"
 
+#include <deque>
+#include <iostream>
+#include <map>
+#include <set>
+#include <vector>
+
+#include "debugging.hpp"
+
+using namespace std;
+
+int max_res;
+int rs_sz;
+int fetch_num;
+
+int cyc = 1;
+int tag = 1;
+int last_tag = -1;
+int done_amt = 0;
+
+struct TraceInfo {
+    int fetch;
+    int disp;
+    int sched;
+    int exec;
+    int state;
+};
+vector<TraceInfo> traces;
+
+vector<proc_inst_t> fetched;
+
+deque<proc_inst_t> dispatch_q;
+
+map<int, set<int>> to_write;
+map<int, set<int>> to_read;
+set<proc_inst_t> rs;
+
+vector<set<pair<int, proc_inst_t>>> fus(3);
+vector<int> fu_free_num;
+
+vector<proc_inst_t> to_delete;
+vector<proc_inst_t> completed;
+
+bool operator<(const proc_inst_t& i1, const proc_inst_t& i2) { return i1.tag < i2.tag; }
+
+ostream& operator<<(ostream& o, const proc_inst_t& i) {
+    return o << "(#" << i.tag << ", " << i.op_code << ": " << i.dest_reg << " <- "
+             << i.src_reg[0] << ", " << i.src_reg[1] << ')';
+}
+
 /**
- * Subroutine for initializing the processor. You many add and initialize any global or heap
- * variables as needed.
+ * Subroutine for initializing the processor. You many add and initialize any global or
+ * heap variables as needed.
  * XXX: You're responsible for completing this routine
  *
  * @r ROB size
@@ -11,31 +60,156 @@
  * @k2 Number of k2 FUs
  * @f Number of instructions to fetch
  */
-void setup_proc(uint64_t r, uint64_t k0, uint64_t k1, uint64_t k2, uint64_t f) 
-{
+void setup_proc(int r, int k0, int k1, int k2, int f) {
+    max_res = r;
+    rs_sz = 2 * (k0 + k1 + k2);
+    fu_free_num = {k0, k1, k2};
+    fetch_num = f;
+}
 
+void fetch() {
+    for (int i = 0; i < fetch_num; i++) {
+        proc_inst_t to_push;
+        if (read_instruction(&to_push)) {
+            to_push.tag = tag++;
+            if (to_push.op_code == -1) { to_push.op_code = 1; }
+            fetched.push_back(to_push);
+            traces.push_back({cyc, -1, -1, -1, -1});
+        } else {
+            last_tag = tag - 1;
+        }
+    }
+}
+
+void dispatch() {
+    for (const proc_inst_t& pi : fetched) {
+        dispatch_q.push_back(pi);
+        traces[pi.tag - 1].disp = cyc;
+    }
+    fetched.clear();
+}
+
+void schedule() {
+    while ((int)rs.size() < rs_sz && !dispatch_q.empty()) {
+        proc_inst_t i = dispatch_q.front();
+        dispatch_q.pop_front();
+        traces[i.tag - 1].sched = cyc;
+
+        // just some neat shorthands
+        int rd = i.dest_reg;
+        int rs1 = i.src_reg[0];
+        int rs2 = i.src_reg[1];
+
+        // no need to check if reg is -1 bc that set is guaranteed to be empty
+        i.rs1_ready = to_write[rs1].empty();
+        i.rs2_ready = to_write[rs2].empty();
+        i.rd_ready = to_read[rd].empty();
+
+        if (rs1 != -1) { to_read[rs1].insert(i.tag); }
+        if (rs2 != -1) { to_read[rs2].insert(i.tag); }
+        if (rd != -1) { to_write[rd].insert(i.tag); }
+
+        rs.insert(i);
+    }
+}
+
+void execute() {
+    for (const proc_inst_t& pi : rs) {
+        if (pi.running) { continue; }
+        set<pair<int, proc_inst_t>>& fu = fus[pi.op_code];
+        // cout << pi << endl;
+        // cout << pi.rs1_ready << ' ' << pi.rs2_ready << ' ' << to_write[pi.src_reg[1]]
+        //      << endl;
+        // cout << fu.size() << endl;
+        bool reg_good = pi.rs1_ready && pi.rs2_ready && pi.rd_ready;
+        if (reg_good && (int)fu.size() < fu_free_num[pi.op_code]) {
+            fu.insert({cyc, pi});
+            traces[pi.tag - 1].exec = cyc;
+            pi.running = true;
+        }
+    }
+}
+
+void state_upd() {
+    for (int i = 0; i < max_res; i++) {
+        // there's almost certainly a better way to do this i just cba
+        pair<pair<int, proc_inst_t>, int> best{{INT32_MAX, {}}, 0};
+        for (int i = 0; i < (int)fus.size(); i++) {
+            if (!fus[i].empty()) { best = min(best, {*fus[i].begin(), i}); }
+        }
+
+        if (best.first.first == INT32_MAX) { break; }
+
+        auto it = fus[best.second].begin();
+        fus[best.second].erase(it);
+
+        proc_inst_t done = it->second;
+        traces[done.tag - 1].state = cyc;
+        completed.push_back(done);
+        done_amt++;
+
+        // nothing wrong w/ erasing an element from an empty set :clueless:
+        to_write[done.dest_reg].erase(done.tag);
+        to_read[done.src_reg[0]].erase(done.tag);
+        to_read[done.src_reg[1]].erase(done.tag);
+    }
+
+    for (const proc_inst_t& i : rs) {
+        int rs1 = i.src_reg[0];
+        i.rs1_ready = to_write[rs1].empty() || i.tag <= *to_write[rs1].begin();
+
+        int rs2 = i.src_reg[1];
+        i.rs2_ready = to_write[rs2].empty() || i.tag <= *to_write[rs2].begin();
+    
+        int rd = i.dest_reg;
+        i.rd_ready = to_read[rd].empty() || i.tag <= *to_read[rd].begin();
+    }
 }
 
 /**
  * Subroutine that simulates the processor.
- *   The processor should fetch instructions as appropriate, until all instructions have executed
- * XXX: You're responsible for completing this routine
+ *   The processor should fetch instructions as appropriate, until all instructions have
+ * executed
  *
  * @p_stats Pointer to the statistics structure
  */
-void run_proc(proc_stats_t* p_stats)
-{
+void run_proc(proc_stats_t* p_stats) {
+    while (last_tag == -1 || done_amt < last_tag) {
+        // for (int i = 0; i < 100; i++) {
+        // cout << cyc << endl;
+        // cout << "state update" << endl;
+        state_upd();
+        // cout << "executing" << endl;
+        execute();
+        // cout << "schedule" << endl;
+        schedule();
+        // cout << "dispatching" << endl;
+        dispatch();
+        // cout << "fetching" << endl;
+        fetch();
 
+        // why is this done at the end bro
+        for (const proc_inst_t& i : to_delete) { rs.erase(i);}
+        to_delete = completed;
+        completed = {};
+
+        cyc++;
+    }
+
+    printf("INST\tFETCH\tDISP\tSCHED\tEXEC\tSTATE\n");
+    for (int i = 0; i < (int)traces.size(); i++) {
+        const TraceInfo& ti = traces[i];
+        printf("%i\t%i\t%i\t%i\t%i\t%i\n", i + 1, ti.fetch, ti.disp, ti.sched, ti.exec,
+               ti.state);
+    }
+    printf("\n");
 }
 
 /**
- * Subroutine for cleaning up any outstanding instructions and calculating overall statistics
- * such as average IPC, average fire rate etc.
+ * Subroutine for cleaning up any outstanding instructions and calculating overall
+ * statistics such as average IPC, average fire rate etc.
  * XXX: You're responsible for completing this routine
  *
  * @p_stats Pointer to the statistics structure
  */
-void complete_proc(proc_stats_t *p_stats) 
-{
-
-}
+void complete_proc(proc_stats_t* p_stats) { p_stats->cycle_count = cyc; }
--- proj3/procsim.hpp
+++ proj3/procsim.hpp
@@ -10,19 +10,21 @@
 #define DEFAULT_R 8
 #define DEFAULT_F 4
 
-typedef struct _proc_inst_t
-{
+typedef struct _proc_inst_t {
     uint32_t instruction_address;
     int32_t op_code;
     int32_t src_reg[2];
     int32_t dest_reg;
-    
-    // You may introduce other fields as needed
-    
+
+    // stack overflow said i could do this
+    mutable bool rs1_ready;
+    mutable bool rs2_ready;
+    mutable bool rd_ready;
+    mutable bool running = false;
+    int tag;
 } proc_inst_t;
 
-typedef struct _proc_stats_t
-{
+typedef struct _proc_stats_t {
     float avg_inst_retired;
     float avg_inst_fired;
     float avg_disp_size;
@@ -33,7 +35,7 @@ typedef struct _proc_stats_t
 
 bool read_instruction(proc_inst_t* p_inst);
 
-void setup_proc(uint64_t r, uint64_t k0, uint64_t k1, uint64_t k2, uint64_t f);
+void setup_proc(int r, int k0, int k1, int k2, int f);
 void run_proc(proc_stats_t* p_stats);
 void complete_proc(proc_stats_t* p_stats);
 
